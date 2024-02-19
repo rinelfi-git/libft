8958ce89b3ee1393737475e0c5c40c6fa0e2824a
Rijaniaina Elie Fidèle <elierijaniaina@gmail.com>
2024-02-19 12:51:22 +0300

    ft_is_alpha

---
+++ ft_isalpha.c
@@ -0,0 +1,5 @@
+#include "libft.h"
+int	ft_isalpha(char c)
+{
+	return (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z');
+}
---
+++ libft.h
@@ -0,0 +1,12 @@
+#ifndef LIBFT
+#define LIBFT 0
+	int		ft_islower(char c);
+	int		ft_isupper(char c);
+	int		ft_isalpha(char c);
+	int		ft_isdigit(char c);
+	int		ft_isalnum(char c);
+	int		ft_isascii(char c);
+	int		ft_isprint(char c);
+	int		ft_strlen(char c);
+	void	*memset(void *s, );
+#endif
