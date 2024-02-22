0396f32c3a54e41fae5e4984fe9e3c99391a4bcc
Rijaniaina Elie Fidèle <elierijaniaina@gmail.com>
2024-02-22 18:57:29 +0300

    ft_memcmp

---
+++ ft_memcmp.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: erijania <erijania@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/22 18:51:06 by erijania          #+#    #+#             */
+/*   Updated: 2024/02/22 18:54:54 by erijania         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	unsigned char	*p_s1;
+	unsigned char	*p_s2;
+
+	p_s1 = (unsigned char *) s1;
+	p_s2 = (unsigned char *) s2;
+	while (n > 0)
+	{
+		if (*p_s1 != *p_s2)
+			return (*p_s1 - *p_s2);
+		p_s1++;
+		p_s2++;
+		n--;
+	}
+	return (0);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: erijania <erijania@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 08:50:14 by erijania          #+#    #+#             */
-/*   Updated: 2024/02/22 18:44:04 by erijania         ###   ########.fr       */
+/*   Updated: 2024/02/22 18:50:37 by erijania         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,4 +33,5 @@ char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 #endif
