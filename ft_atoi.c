72534ed57e1248d6d5e17c58bde5f8629e48d660
Rijaniaina Elie Fidèle <elierijaniaina@gmail.com>
2024-02-22 19:24:45 +0300

    ft_atoi

---
+++ ft_atoi.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: erijania <erijania@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/22 19:10:30 by erijania          #+#    #+#             */
+/*   Updated: 2024/02/22 19:24:38 by erijania         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_atoi(const char *nptr)
+{
+	int	ret;
+	int	sign;
+
+	sign = 1;
+	ret = 0;
+	while (*nptr == ' ')
+		nptr++;
+	if (*nptr == '+' || *nptr == '-')
+	{
+		if (*nptr == '-')
+			sign = -1;
+		nptr++;
+	}
+	while (*nptr && ft_isdigit(*nptr))
+		ret = ret * 10 + ((int) *(nptr++) - '0');
+	return (ret * sign);
+}
