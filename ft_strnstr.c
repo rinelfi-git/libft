f0816bb0664c1bdd9b9f3af1444872705d8f4638
Rijaniaina Elie Fidèle <elierijaniaina@gmail.com>
2024-02-22 19:09:29 +0300

    ft_strnstr

---
+++ ft_strnstr.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: erijania <erijania@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/22 19:01:03 by erijania          #+#    #+#             */
+/*   Updated: 2024/02/22 19:09:08 by erijania         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *big, const char *little, size_t len)
+{
+	size_t	little_len;
+
+	little_len = ft_strlen(little);
+	if (little_len == 0)
+		return ((char *) big);
+	while (*big && len >= little_len)
+	{
+		if (ft_strncmp(big, little, len) == 0)
+			return ((char *) big);
+		big++;
+		len--;
+	}
+	return (NULL);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: erijania <erijania@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 08:50:14 by erijania          #+#    #+#             */
-/*   Updated: 2024/02/22 18:50:37 by erijania         ###   ########.fr       */
+/*   Updated: 2024/02/22 19:03:11 by erijania         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,4 +34,5 @@ char	*ft_strrchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
+char	*ft_strnstr(const char *big, const char *little, size_t len);
 #endif
